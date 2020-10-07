81260efb53159c4c9614a4d937c026b9e29e495a
ImLordImpaler <40574240+ImLordImpaler@users.noreply.github.com>
2020-10-07 23:53:23 +0530

    Update settings.py

--- seraphFinal/settings.py
+++ seraphFinal/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '7_o$q99)2@=#gu0y8i7$5d#$b8-0037dpw+28&-j)5b=pr3!b^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['dhanya.herokuapp.com']
 
 
 # Application definition
